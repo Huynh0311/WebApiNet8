a77a2813935277b35b7affbbda9e17510ad2e0d2
huynh0311 <huynhtv0311@gmail.com>
2024-12-04 16:44:42 +0700

    Add project files.

---
+++ WebApiNet8/Controllers/EmailsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiNet8.Controllers {
+    [Route("api/emails")]
+    [ApiController]
+    public class EmailsController : ControllerBase {
+        public EmailsController() { }
+
+        [HttpGet()]
+        public IActionResult Get() {
+
+            return Ok("Test api");
+        }
+
+        [HttpGet("test")]
+        public IActionResult Get() {
+
+            return Ok("Test api");
+        }
+    }
+
+}
---
+++ WebApiNet8/Services/ILarkServices.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiNet8.Services {
+    public interface ILarkServices {
+        public string GetToken();
+
+        public Task GetDataBase();
+
+    }
+}
